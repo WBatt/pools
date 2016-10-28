cd09c7da78ab5db22acb254d9f291a68c8e2c8ee
Will <b4ttm4n@gmail.com>
2016-10-28 06:48:26 +0000

    changed font, fixed phone for mobile, added some js

---
+++ main.js
@@ -0,0 +1,9 @@
+function closePhone() {
+    $('.caller').addClass("hide");
+    $('.phone').addClass("show");
+}
+
+function openPhone() {
+    $('.caller').removeClass("hide");
+    $('.phone').removeClass("show");
+}
